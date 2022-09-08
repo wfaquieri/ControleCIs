79a37efc4f0914155ec0a8b262f5907f12eb77a3
Winicius Faquieri <54724688+wfaquieri@users.noreply.github.com>
2022-09-08 10:57:39 -0300

    att

--- data-raw/DATASET.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-controle = readxl::read_excel("data-raw/controle.xlsx",
-                              .name_repair = janitor::make_clean_names)
-
-controle |> dplyr::glimpse()
-
-usethis::use_data(controle, overwrite = TRUE)
